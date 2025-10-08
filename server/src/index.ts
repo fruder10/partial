4b75b2c70f8b979490dcd28773cd7a0c3737cff8
fruder10 <fruder10@gmail.com>
2025-10-07 22:45:40 -0700

    server modifications

--- server/src/index.ts
+++ server/src/index.ts
@@ -50,7 +50,7 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 });
 
 /* SERVER */
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const port = Number(process.env.PORT) || 3000;
+app.listen(port, "0.0.0.0", () => {
     console.log(`Server running on port ${port}`);
 });
